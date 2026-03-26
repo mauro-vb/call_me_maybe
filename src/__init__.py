cfbb3df760ddc643cc1d4930e5cfc0c6a5dbbb7e
Mauro Vazquez Bassat <mvazquez@car1s2.42barcelona.com>
2026-03-26 13:31:43 +0100

    added llm_sdk to workspace

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'mauro-vb'
+__version__ = '1.0.0'
+
+# LLM SDK
+from llm_sdk import Small_LLM_Model
+
+
+__all__ = [
+    'Small_LLM_Model'
+]
--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,9 @@
-
+from src import Small__LLM_Model
 
 def main() -> None:
     print("Executing main...")
+    model = Small__LLM_Model()
+    print(model.encode("Hello world!"))
 
 
 if __name__ == '__main__':
